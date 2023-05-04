3c0405fe67fd9c502c2351038a482da512edc7a6
Abdrahman Moussa <maloom7>
2023-05-04 17:16:36 +0300

    adding coins search

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -8,10 +8,26 @@ const Cryptocurrencies = ({simplified}) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   // this line giving us access to coins
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+      const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+       
+      setCryptos(filteredData);
+      }, [cryptosList, searchTerm]);
+
   if(isFetching) return 'Loading...';
   return (
     <>
+    {!simplified && (
+       <div className="search-crypto">
+       <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+
+      </div>
+
+ )}
+      
     <Row gutters={[32,32]} className="crypto-card-container">
       {cryptos?.map((currency) =>(
         <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
